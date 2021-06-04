e4ccf79b56ba1fd7fa0892b162d08b0865296752
Song Myung Kyu <singsong@paran.com>
2021-06-04 14:33:25 +0000

    class validator update 6/4

--- src/rest/dto/create-rest.dto.ts
+++ src/rest/dto/create-rest.dto.ts
@@ -1,17 +1,22 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsString, IsBoolean } from 'class-validator';
 
 @ArgsType () 
 export class CreateRestDTO {
     @Field(()=>String) 
+    @IsString()
     name : string;
     
     @Field(()=>Boolean) 
+    @IsBoolean()
     isVegan : boolean;
     
     @Field(()=>String) 
+    @IsString()
     address : string;
     
     @Field(()=>String) 
+    @IsString()
     ownerName : string;
     
 }
